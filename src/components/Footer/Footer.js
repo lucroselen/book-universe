3a65a411ba4ea542e79f422d4b8067f775e5f491
lucroselen <mriliev@outlook.com>
2021-11-22 01:27:05 +0200

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer className="footer navbar-light bg-light">
+      <div>Book Universe 2021</div>
+    </footer>
+  );
+};
+
+export default Footer;
