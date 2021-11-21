19a26d2f80dc6984b77d25f3883cdd1be71e1970
lucroselen <mriliev@outlook.com>
2021-11-22 01:27:31 +0200

    added components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,17 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./components/Login/Login";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+//import { useState, useEffect } from "react";
+//import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <span className="app-navbar navbar-brand">Book Universe</span>
-
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Top 10 books
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Worst ranked books
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  My Profile
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Login
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
-      <p>Hello World!</p>
-      <footer className="footer">
-        <div>Book Universe 2021</div>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <Login />
+      <Footer />
+    </>
   );
 }
 
