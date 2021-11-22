eae77de6f2bfec0702d16b2028cb5b2a4afde3f8
lucroselen <mriliev@outlook.com>
2021-11-23 00:55:41 +0200

    css touch ups

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -7,10 +7,7 @@ const Register = () => {
       <div className="container py-5 h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div
-              className="card bg-dark text-white"
-              style={{ borderRadius: "1rem" }}
-            >
+            <div className="card bg-dark text-white card-border">
               <div className="card-body p-5 text-center">
                 <div className="mb-md-5 mt-md-4 pb-5">
                   <h2 className="fw-bold mb-2 text-uppercase">Register</h2>
