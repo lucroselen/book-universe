e3acd7b11db80687f2c62af67e869a5d26aff082
lucroselen <mriliev@outlook.com>
2021-12-04 22:17:21 +0200

    button on homepage - auth or not auth

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,20 @@
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
 const Home = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div>
       <section id="home">
         <div className="home-container">
           <h1>Welcome to Book Universe</h1>
           <h2>Here you can find your next favourite book!</h2>
-          <Link to="/all-books" className="btn-get-started">
+          <Link
+            to={user.id ? "/all-books" : "/register"}
+            className="btn-get-started"
+          >
             Get Started
           </Link>
         </div>
