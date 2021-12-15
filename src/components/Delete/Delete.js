5a2bfa38642084ae84f4f4ee6bed4fadc58751a4
lucroselen <mriliev@outlook.com>
2021-12-16 01:15:43 +0200

    added delete notifications

--- src/components/Delete/Delete.js
+++ src/components/Delete/Delete.js
@@ -2,14 +2,24 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import * as mainService from "../../services/mainService";
 import { useEffect } from "react";
+import { useNotificationContext } from "../../contexts/NotificationContext";
 
 const Delete = () => {
+  const { addNotification } = useNotificationContext();
+
   const { bookId } = useParams();
   const navigate = useNavigate();
   useEffect(() => {
-    mainService.deleteOne(bookId).then(() => {
-      navigate("/all-books");
-    });
+    mainService
+      .deleteOne(bookId)
+      .then(() =>
+        addNotification("Book deleted successfully!", "alert-success")
+      )
+      .then(() => navigate("/all-books"))
+      .catch((error) => {
+        addNotification(error.error, "alert-danger");
+        navigate(`/details/${bookId}`);
+      });
   });
 
   return null;
