d1d90abd879c364cc9b23adc787a8120d1f1d9dc
lucroselen <mriliev@outlook.com>
2021-12-16 03:35:25 +0200

    created separate css file

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -1,10 +1,6 @@
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import React, { Component } from "react";
-
-const mapStyles = {
-  width: "100%",
-  height: "100%",
-};
+import "./MapContainer.css";
 
 export class MapContainer extends Component {
   render() {
@@ -12,7 +8,7 @@ export class MapContainer extends Component {
       <Map
         google={this.props.google}
         zoom={12}
-        style={mapStyles}
+        className="maps"
         initialCenter={{ lat: 42.63612256076243, lng: 23.36976168577798 }}
       >
         <Marker position={{ lat: 42.63612256076243, lng: 23.36976168577798 }} />
